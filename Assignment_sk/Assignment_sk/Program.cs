05d383dd370c2e2ba7d94c19f0636bdb5601aad4
sravanichintala <sravanichintala2466@gmail.com>
2022-01-05 19:51:56 +0530

    Added file

---
+++ Assignment_sk/Assignment_sk/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_sk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StudentDetails SravaniStudentDetails = new StudentDetails(1001, "Sravani", "c#");
+            StudentDetails JanuStudentDetails = new StudentDetails(1002, "Janu", "ASP.NET");
+
+
+            SravaniStudentDetails.Payment(200);
+            JanuStudentDetails.Payment(100);
+
+
+            SravaniStudentDetails.Print();
+
+            Console.WriteLine("\n");
+
+
+            JanuStudentDetails.Print();
+        }
+    }
+}
