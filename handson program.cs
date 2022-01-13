c539bfc15ed5ce772cab525c7c9d2ba700a8db70
sravanichintala <sravanichintala2466@gmail.com>
2022-01-13 18:07:01 +0530

    add the files

---
+++ handson program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int width = num;
+
+            int height = width;
+            for (int row = 0; row < height; row++)
+            {
+                for (int column = 0; column < width; column++)
+                {
+                    Console.Write(num);
+                }
+
+                Console.WriteLine();
+                width--;
+            }
+        }
+    }
+}
